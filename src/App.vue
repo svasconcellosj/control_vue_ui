2ab31762eb9c0b9446bdde4eef476640218c5aec
Sergio Vasconcellos Junior <svasconcellosj@hotmail.com>
2022-04-30 17:31:51 -0300

    Incluído Bavbar
    - Incluído barra de menu superior
    - Ajustado porta do servidor
    - Instalado Primevue
    - Instalado Primeicons
    - Instalado Primeflex

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <span>
+    <header>
+      <nav-bar logo="Controle" url="#" background="var(--primary-color)">
+        <li><a href="sass.html">Dashboard</a></li>
+        <li><a href="collapsible.html">Usuário</a></li>
+      </nav-bar>
+    </header>
+    <main>
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+    <footer />
+  </span>
 </template>
 
 <script>
+import NavBar from "@/components/layouts/NavBar";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    NavBar
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --secundary-color: var(--blue-800);
+  --hover-color: var(--blue-300);
 }
+* {
+  margin: 0;
+  padding: 0;
+
+  font-family: var(--font-family);
+}
+
+a {
+  text-decoration: none;
+}
+
+@media (min-width: 1200px) {
+  .container {
+    width: 1170px;
+    margin: 0 auto;
+  }
+}
+
+.centraliza {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+import 'primeflex/primeflex.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App,
+    PrimeVue
+  },
   template: '<App/>'
 })
